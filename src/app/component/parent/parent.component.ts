25ebf86d99e5d92b660bcd5c1e5217f7f1e1467b
takwamn <takwa.manai@sesame.com.tn>
2023-11-22 16:01:15 +0100

    commit dynamicForm

--- src/app/component/parent/parent.component.ts
+++ src/app/component/parent/parent.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators } from '@angular/forms';
-import { ApiResponse } from 'src/app/model/ApiResponse';
-import { Devise } from 'src/app/model/devise';
 import { FormData } from '../../model/FormData';
-import { JsonService } from '../../service/json.service';
 
 @Component({
   selector: 'app-parent',
@@ -12,8 +9,6 @@ import { JsonService } from '../../service/json.service';
 })
 
 export class ParentComponent implements OnInit {
-
-  dataSourceDevise: Devise[] = [];
 
   fieldOptions: { [key: string]: string[] } = {
     'select': [],
@@ -30,26 +25,16 @@ export class ParentComponent implements OnInit {
     return this.fieldOptions[fieldType] || [];
   }
 
-  constructor(private jsonService: JsonService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getAllDevise();
     this.initDynamicFormData();
 
 
   }
 
   public add(formData) {
-    console.log('Form data to add:', formData);
-
-    this.jsonService.add(formData).subscribe(
-      (resp) => {
-        console.log('Réponse de l\'ajout:', resp);
-      },
-      (err) => {
-        console.log('Erreur lors de l\'ajout:', err);
-      }
-    );
+    console.log('Form data to add:', formData);     
   }
 
 
@@ -70,40 +55,22 @@ export class ParentComponent implements OnInit {
     return {};
   }
 
-  getAllDevise() {
-    this.jsonService.getAll().subscribe((res: ApiResponse<Devise>) => {
-      console.log('%getAlldevise() line:70  ', 'color: #007acc;', res.data);
-      this.dataSourceDevise = res.data.map((item: Devise) => Devise.fromJson(item));
-
-      this.key = "id";
-      this.value = "dvsCode";
-
-      this.initDynamicFormData();
-
-
-    });
-  }
-
-
-
   initDynamicFormData() {
     this.dynamicFormData = {
       fields: [
-        { description: "id", type: 'text', placeholder: "id", label: 'id', hidelabel: false, disabled: false, hidden: false, ...this.getValidators('id'), defaultValue: "", options: [], selectedValue: null, key: "", value: "" },
-        { description: "Code", type: 'text', placeholder: "Code", label: 'paysCod', hidelabel: false, disabled: false, hidden: false, ...this.getValidators('paysCod'), defaultValue: "", options: [], selectedValue: null, key: "", value: "" },
-        { description: "Description", type: 'text', placeholder: "Description", hidelabel: false, label: 'paysDesc', disabled: false, hidden: false, ...this.getValidators('paysDesc'), defaultValue: "", options: [], selectedValue: null, key: "", value: "" },
-        { description: "Abréviation", type: 'number', placeholder: "Abréviation", hidelabel: false, label: 'paysAbr', disabled: true, hidden: false, defaultValue: 40, options: [], selectedValue: null, key: "", value: "" },
-        { description: "Statut", type: 'text', placeholder: "Statut", label: 'paysStatus', hidelabel: false, disabled: true, hidden: false, defaultValue: "CRE", ...this.getValidators('paysStatus'), options: [], selectedValue: null, key: "", value: "" },
-        { description: "Devise", type: 'select', placeholder: "Devise", label: 'Devise', hidelabel: false, disabled: false, hidden: false, defaultValue: "", options: this.dataSourceDevise, selectedValue: "", key: this.key, value: this.value },
-
-       // { type: 'textarea', label: 'commentaire', rows: 5, disabled: false, hidden: false, defaultValue: "", options: [], selectedValue: null },
-       // { type: 'select', label: 'genre', disabled: false, hidden: false, defaultValue: "", options: ['Masculin', 'Féminin', 'Autre'], selectedValue: 'Féminin' },
-        { description: "accepter", type: 'checkbox', placeholder: "accepter", label: 'accepter', hidelabel: false, disabled: false, hidden: false, defaultValue: false, options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'], selectedValue: 'Option 1', key: "", value: "" },
-        { description: "choix", type: 'radio', placeholder: "choix", label: 'choix', hidelabel: false, disabled: false, hidden: false, defaultValue: false, options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'], selectedValue: 'Option 1', key: "", value: "" },
+        { description: "Identifiant", type: 'text', placeholder: "Identifiant", label: 'identifiant', hidelabel: false, disabled: false, hidden: false, ...this.getValidators('identifiant'), defaultValue: "", options: [], selectedValue: null, key: "", value: "" },
+        { description: "Nom", type: 'text', placeholder: "Nom", label: 'nomEtudiant', hidelabel: false, disabled: false, hidden: false, ...this.getValidators('nomEtudiant'), defaultValue: "", options: [], selectedValue: null, key: "", value: "" },
+        { description: "Âge", type: 'number', placeholder: "Âge", hidelabel: false, label: 'ageEtudiant', disabled: false, hidden: false, ...this.getValidators('ageEtudiant'), defaultValue: "", options: [], selectedValue: null, key: "", value: "" },
+        { description: "Moyenne", type: 'number', placeholder: "Moyenne", hidelabel: false, label: 'moyenneEtudiant', disabled: true, hidden: false, defaultValue: 40, options: [], selectedValue: null, key: "", value: "" },
+        { description: "Adresse", type: 'text', placeholder: "Adresse", label: 'adresseEtudiant', hidelabel: false, disabled: true, hidden: false, defaultValue: "Tunis", ...this.getValidators('adresseEtudiant'), options: [], selectedValue: null, key: "", value: "" },
+        { description: "Cours", type: 'select', placeholder: "Cours", label: 'coursEtudiant', hidelabel: false, disabled: false, hidden: false, defaultValue: "", options: [], selectedValue: "", key: this.key, value: this.value },
+        { description: "Accepter", type: 'checkbox', placeholder: "Accepter", label: 'accepter', hidelabel: false, disabled: false, hidden: false, defaultValue: false, options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'], selectedValue: 'Option 1', key: "", value: "" },
+        { description: "Genre", type: 'radio', placeholder: "Genre", label: 'genre', hidelabel: false, disabled: false, hidden: false, defaultValue: false, options: ['Homme', 'Femme'], selectedValue: 'Homme', key: "", value: "" },
       ], columnsPerRow: 2,
     };
-
   }
+  
+
 
 }
 
